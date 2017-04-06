0f8ad1d242dbfe29f5f308a3784d7ab7d08ecc4b
root <joaoded@hotmail.com>
2017-04-06 18:07:07 -0300

    Inclusions show action ok

--- app/controllers/inclusions_controller.rb
+++ app/controllers/inclusions_controller.rb
@@ -1,4 +1,8 @@
 class InclusionsController < ApplicationController
+	def show
+		@inclusion = Inclusion.find(params[:id])
+	end	
+
 	def new
 	end
 	
@@ -6,12 +10,12 @@ class InclusionsController < ApplicationController
 		@inclusion = Inclusion.new(inclusion_params)
 
 		@inclusion.save
-  	redirect_to @inclusion
-	end
+  		redirect_to @inclusion
+	end	
 
 	private 
 		def inclusion_params
 			params.require(:inclusion).permit(:title, :text)
-		end		
-
+		end
+		
 end
