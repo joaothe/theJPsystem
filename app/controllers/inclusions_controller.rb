e4aa54b2cd55f2429d9e917320de8a1f2afd2e39
root <joaoded@hotmail.com>
2017-04-19 16:34:09 -0300

    Sending some validation and others implementations

--- app/controllers/inclusions_controller.rb
+++ app/controllers/inclusions_controller.rb
@@ -1,23 +1,27 @@
 class InclusionsController < ApplicationController
 	def index
 		@inclusions = Inclusion.all
-	end	 
+	end
 
 	def show
 		@inclusion = Inclusion.find(params[:id])
-	end	
+	end
 
 	def new
+		@inclusion = Inclusion.new
 	end
-	
+
 	def create
 		@inclusion = Inclusion.new(inclusion_params)
 
-		@inclusion.save
+		if @inclusion.save
   		redirect_to @inclusion
-	end	
+		else
+			render 'new'
+		end
+	end
 
-	private 
+	private
 		def inclusion_params
 			params.require(:inclusion).permit(:title, :text)
 		end
