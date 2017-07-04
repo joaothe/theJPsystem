c232244867ccfc57718580065e2fef247ba9a739
root <joaoded@hotmail.com>
2017-07-04 15:59:33 -0300

    Implementing basic authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+   http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
   def create
     @inclusion = Inclusion.find(params[:inclusion_id])
     @comment = @inclusion.comments.create(comment_params)
--- app/controllers/inclusions_controller.rb
+++ app/controllers/inclusions_controller.rb
@@ -1,4 +1,7 @@
 class InclusionsController < ApplicationController
+
+	http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
 	def index
 		@inclusions = Inclusion.all
 	end
